2383e9bbd136732f4c76a15fe837472e3867905c
rodrigo.merigueti <rod.merigueti@gmail.com>
2025-09-28 17:56:11 -0300

    feat(main): add initial project structure with main application logic and build configuration

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include "SDL3/SDL.h"
+#include "SDL3/SDL_render.h"
+#include "SDL3/SDL_video.h"
+
+auto init(SDL_Window **window, SDL_Renderer **renderer) -> auto {
+  if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS)) {
+    return false;
+  }
+
+  bool success = SDL_CreateWindowAndRenderer(
+      "Lunar", 256, 224, SDL_WINDOW_RESIZABLE, window, renderer);
+
+      if (success == false) {
+    SDL_Log("ERROR!");
+    return false;
+  }
+  SDL_Log("START - GAME");
+  return true;
+}
+
+int main(int /*argc*/, char* /*argv*/[]) {
+  SDL_Window *window;
+  SDL_Renderer *renderer;
+  int color = 0;
+
+  if (init(&window, &renderer) == false) {
+    return -1;
+  }
+
+
+  bool should_run{true};
+
+  while (should_run) {
+    SDL_Event e;
+
+    if (SDL_PollEvent(&e)) {
+
+      if (e.type == SDL_EVENT_QUIT) {
+        should_run = false;
+      }
+      if (e.type == SDL_EVENT_KEY_DOWN && e.key.key == SDLK_ESCAPE) {
+        should_run = false;
+      }
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
+
+  // **************************************************************
+
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}
